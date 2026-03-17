bd54662f8f68a4bbe407b171ebfa0febf14db946
Zoom <xabou2010@gmail.com>
2026-03-16 21:21:53 -0600

    refactor(output): got tired of coding whole json lines just to get an output so made this to simplify my life

---
+++ internal/output/json.go
@@ -0,0 +1,16 @@
+package output
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func PrintJSON(v any) error {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(b))
+	return nil
+}
