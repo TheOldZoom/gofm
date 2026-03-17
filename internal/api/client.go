10847b25aa4239586d2bcba0ed55398e0877856b
Zoom <xabou2010@gmail.com>
2026-03-16 21:25:26 -0600

    refactor(api): updated browser agent to latest

--- internal/api/client.go
+++ internal/api/client.go
@@ -9,7 +9,7 @@ import (
 )
 
 const BaseURL = "https://ws.audioscrobbler.com/2.0/"
-const browserUserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
+const browserUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36"
 
 type Client struct {
 	ApiKey string
